cb2ef189cf19d1cea491ac3e9a981fe693926054
Nahuel <nahueltecnicomdp@gmail.com>
2025-08-15 00:28:16 -0300

    a

--- src/app/(game)/final/page.tsx
+++ src/app/(game)/final/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function FinalPage() {
   return (
     <div className="min-h-screen w-full  bg-[url('/final-bg.png')] bg-cover bg-center mx-auto flex flex-col items-center justify-end gap-8 py-8 px-6">
-      <p className="text-5xl font-semibold">
-        Buscá tu silla <br /> y a tu equipo <br /> para el <i>hackaton</i>
+      <p className="text-5xl font-semibold whitespace-nowrap">
+        Buscá tu silla <br /> y a tu equipo <br /> para el <i>hackaton.</i>
       </p>
 
       <Image
