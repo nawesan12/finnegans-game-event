60f02668bfc3631def72f03f73de7ded72c6b0a8
Nahuel <nahueltecnicomdp@gmail.com>
2025-08-15 00:17:09 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
-import RedirectManager from "./RedirectManager";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <RedirectManager />;
+  return (
+    <section className="bg-[#04102d] text-white min-h-screen flex flex-col items-center justify-center gap-4 p-4">
+      <h1 className="text-4xl">
+        Comienza un <br />{" "}
+        <span className="text-[#4bc3fe]">viaje a lo alto</span>
+      </h1>
+      <div>
+        <Image
+          src="/pasaporte.png"
+          alt="Logo"
+          width={400}
+          height={400}
+          className="max-h-[500px] object-contain"
+        />
+      </div>
+
+      <p className="text-[#4bc3fe] text-4xl">¿venís?</p>
+
+      <div className="flex items-center justify-center gap-4">
+        <Link href="/estaciones">
+          <Image
+            src="/boton-obvio.png"
+            alt="Logo"
+            width={200}
+            height={200}
+            className="h-20 object-contain"
+          />
+        </Link>
+        <Link href="/estaciones">
+          <Image
+            src="/boton-supuesto.png"
+            alt="Logo"
+            width={200}
+            height={200}
+            className="h-20 object-contain"
+          />
+        </Link>
+      </div>
+    </section>
+  );
 }
